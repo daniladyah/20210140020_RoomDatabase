a2948a18e5ccf71e4b28078afe00b7aea579f0d5
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-05 16:37:28 +0700

    menambahkan data input id,nama, alamat dan telpom

--- app/src/main/java/com/example/roomsiswa/ui/theme/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/model/EntryViewModel.kt
@@ -46,5 +46,15 @@ fun DetailSiswa.toSiswa(): Siswa = Siswa(
     alamat = alamat,
     telpon = telpon
 )
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
 
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
 
