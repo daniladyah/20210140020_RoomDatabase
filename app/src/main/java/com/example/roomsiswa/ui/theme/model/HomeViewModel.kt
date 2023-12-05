6d7cf56a61e66d7c4e49f4782831d70de27a7a88
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-05 16:40:57 +0700

    menambahkan Home view model

--- app/src/main/java/com/example/roomsiswa/ui/theme/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/model/HomeViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.roomsiswa.ui.theme.model
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.RepositoriSiswa
+
+class HomeViewModel(private  val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
 }
