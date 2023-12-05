a55f17b17962b7f44bbec53b666be1e06997e6a8
Danila Dyah <danila.dyah.ft21@mail.umy.ac.id>
2023-12-05 16:51:40 +0700

    menambahkan ekstensi queri untuk objek

--- app/src/main/java/com/example/roomsiswa/ui/theme/model/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/ui/theme/model/PenyediaViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.roomsiswa.data
 
 import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.roomsiswa.AplikasiSiswa
 import com.example.roomsiswa.ui.theme.model.HomeViewModel
 
 object PenyediaViewModel{
@@ -12,3 +15,6 @@ object PenyediaViewModel{
         }
     }
 }
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
